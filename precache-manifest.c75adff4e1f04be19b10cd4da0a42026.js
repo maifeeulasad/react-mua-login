4bf5ecc85c4c8eb7ac2817792484ba7383102013
maifeeulasad <maifeeulasad@gmail.com>
2020-08-01 14:58:07 +0600

    gh-pages

---
+++ precache-manifest.c75adff4e1f04be19b10cd4da0a42026.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fef3caf522ef8133828ae2f474bd5520",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4f7f575c02398c26aaad",
+    "url": "./static/css/main.0f8a65ef.chunk.css"
+  },
+  {
+    "revision": "77e2fc74e82322f6b311",
+    "url": "./static/js/2.ba43eca8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ba43eca8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f7f575c02398c26aaad",
+    "url": "./static/js/main.c319ddaa.chunk.js"
+  },
+  {
+    "revision": "ad4c336ade948ed35d15",
+    "url": "./static/js/runtime-main.a2159c0d.js"
+  },
+  {
+    "revision": "e4a33821d41dc6bd16c63b42be740f36",
+    "url": "./static/media/login~bhwauFWB.e4a33821.svg"
+  }
+]);
